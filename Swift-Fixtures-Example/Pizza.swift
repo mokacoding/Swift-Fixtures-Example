b5c77cc58f292e0fc46e37f7dbfdd899f234f748
Giovanni Lodi <giovanni.lodi42@gmail.com>
2018-10-06 20:21:04 +1000

    Add Pizza 🍕

---
+++ Swift-Fixtures-Example/Pizza.swift
@@ -0,0 +1,33 @@
+//
+//  Pizza.swift
+//  Swift-Fixtures-Example
+//
+//  Created by Gio on 6/10/18.
+//  Copyright © 2018 mokacoding. All rights reserved.
+//
+
+protocol Topping {
+  var isVegetarian: Bool { get }
+}
+
+struct TomatoSauce: Topping {
+  let isVegetarian = true
+}
+
+struct Mozzarella: Topping {
+  let isVegetarian = true
+}
+
+struct Salami: Topping {
+  let isVegetarian = false
+}
+
+struct Pizza {
+
+  let name: String
+  let toppings: [Topping]
+
+  var isVegeterian: Bool {
+    return toppings.contains(where: { $0.isVegetarian == false }) == false
+  }
+}
---
+++ Swift-Fixtures-ExampleTests/PizzaTests.swift
@@ -0,0 +1,25 @@
+//
+//  PizzaTests.swift
+//  Swift-Fixtures-ExampleTests
+//
+//  Created by Gio on 6/10/18.
+//  Copyright © 2018 mokacoding. All rights reserved.
+//
+
+@testable import Swift_Fixtures_Example
+import XCTest
+
+class PizzaTests: XCTestCase {
+
+  func testIsVegetarianWithVegetarianToppings() {
+    let pizza = Pizza(name: "name", toppings: [TomatoSauce(), Mozzarella()])
+
+    XCTAssertTrue(pizza.isVegeterian)
+  }
+
+  func testIsVegetarianWithNonVegetarianToppings() {
+    let pizza = Pizza(name: "name", toppings: [TomatoSauce(), Mozzarella(), Salami()])
+
+    XCTAssertFalse(pizza.isVegeterian)
+  }
+}

a9571040c045d16511f5be047fe953400a51f0e0
Giovanni Lodi <giovanni.lodi42@gmail.com>
2018-10-06 20:08:14 +1000

    Create empty framework via Xcode template

---
+++ Swift-Fixtures-Example/Swift_Fixtures_Example.h
@@ -0,0 +1,17 @@
+//
+//  Swift_Fixtures_Example.h
+//  Swift-Fixtures-Example
+//
+//  Created by Gio on 6/10/18.
+//  Copyright © 2018 mokacoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift_Fixtures_Example.
+FOUNDATION_EXPORT double Swift_Fixtures_ExampleVersionNumber;
+
+//! Project version string for Swift_Fixtures_Example.
+FOUNDATION_EXPORT const unsigned char Swift_Fixtures_ExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Fixtures_Example/PublicHeader.h>
